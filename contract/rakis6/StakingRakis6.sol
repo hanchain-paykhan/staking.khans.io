36a4b833d735d82c60a98617f00b2e03c2e146dc
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-11-21 17:52:08 +0900

    stakingRakis6

--- contract/rakis6/StakingRakis6.sol
+++ contract/rakis6/StakingRakis6.sol
@@ -10,45 +10,24 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 contract StakingRakis6 is ReentrancyGuard, Ownable, Pausable {
     IERC20 public stakingToken; // rakis-6
     IERC20 public rewardToken; // Han
-    IERC20 public wethToken; // WETH
-    IERC20 public usdcToken; // USDC
-    IERC721 public uniV3Token; // Uniswap V3
+
+    constructor(address _stakingToken, address _rewardToken) onlyOwner {
+        stakingToken = IERC20(_stakingToken);
+        rewardToken = IERC20(_rewardToken);
+    }
 
     uint256 public constant hanTokenPerLpToken = 274959775134; // HanToken quantity per LP token
 
     uint256 public totalSupply; // Total amount of tokens staked
     uint256 public tokenVolume = 10000 ether; // Amount of tokens that the user can stake in the contract
+    uint256 public rewardsDuration = 365 days;
 
     mapping(address => uint256) public getAmount; // Amount of tokens staked
     mapping(address => uint256) public getStakingStartTime; // Time Staking Started
     mapping(address => uint256) public getRewardReleased; // The total amount of Han Token received as a reward
     mapping(address => uint256) public getBalance; // Han Token is piled up before making a claim
 
-    constructor(
-        address _stakingToken,
-        address _rewardToken,
-        address _wethToken,
-        address _usdcToken,
-        address _uniV3Token
-    ) onlyOwner {
-        stakingToken = IERC20(_stakingToken);
-        rewardToken = IERC20(_rewardToken);
-        wethToken = IERC20(_wethToken);
-        usdcToken = IERC20(_usdcToken);
-        uniV3Token = IERC721(_uniV3Token);
-    }
-
-    // Staked time lookup function
-    function stakingTimeData() public view returns (uint256) {
-        if (getAmount[msg.sender] == 0) {
-            return block.timestamp;
-        } else {
-            uint256 stakedTime;
-            stakedTime = (block.timestamp -
-                uint256(getStakingStartTime[msg.sender]));
-            return stakedTime;
-        }
-    }
+    // ------------------ Admin ------------------ //
 
     // Staking pause
     function pause() public onlyOwner {
@@ -59,36 +38,14 @@ contract StakingRakis6 is ReentrancyGuard, Ownable, Pausable {
         _unpause();
     }
 
-    /* ==================== Admin ==================== */
+    function setRewardsDuration(uint256 _newDuration) public onlyOwner {
+        rewardsDuration = _newDuration;
+    }
 
     // Token amount modification function that can be staked into a contract
     function setTokenVolume(uint256 _newVolume) public onlyOwner {
         require(_newVolume > totalSupply, "Too Small Volume");
         tokenVolume = _newVolume;
-    }
-
-    // Contract's UNI-V3-TokenIds lookup function
-    function uniTokenBalance() public view returns (uint256) {
-        uint256 tokenIds;
-        tokenIds = uniV3Token.balanceOf(address(this));
-        return tokenIds;
-    }
-
-    // Contract's UNI-V3-TokenIds Transfer Function
-    function transferUniToken(uint256 _tokenId) public onlyOwner {
-        uniV3Token.safeTransferFrom(address(this), msg.sender, _tokenId);
-    }
-
-    // Contract's Han Token lookup function
-    function rewardTokenBalance() public view returns (uint256) {
-        uint256 balance;
-        balance = rewardToken.balanceOf(address(this));
-        return balance;
-    }
-
-    // Contract's Han Token Transfer Function
-    function transferHanToken(uint256 _amount) public onlyOwner {
-        rewardToken.transfer(msg.sender, _amount);
     }
 
     // LP Token lookup function of contract
@@ -114,40 +71,86 @@ contract StakingRakis6 is ReentrancyGuard, Ownable, Pausable {
         stakingToken.transfer(msg.sender, _amount);
     }
 
-    // WETH Token lookup function that contract has
-    function wethTokenBalance() public view returns (uint256) {
-        uint256 balance;
-        balance = wethToken.balanceOf(address(this));
-        return balance;
+    function recoverERC20(address _tokenAddress, uint256 _tokenAmount)
+        external
+        onlyOwner
+    {
+        require(
+            _tokenAddress != address(stakingToken),
+            "Cannot withdraw the staking token"
+        );
+        IERC20(_tokenAddress).transfer(msg.sender, _tokenAmount);
+        emit RecoveredERC20(_tokenAddress, _tokenAmount);
     }
 
-    // WETH Token Transfer Function of Contract
-    function transferWethToken(uint256 _amount) public onlyOwner {
-        wethToken.transfer(msg.sender, _amount);
+    function recoverERC721(address _tokenAddress, uint256 _tokenId)
+        external
+        onlyOwner
+    {
+        require(
+            _tokenAddress != address(stakingToken),
+            "Cannot withdraw the staking token"
+        );
+        IERC721(_tokenAddress).safeTransferFrom(
+            address(this),
+            msg.sender,
+            _tokenId
+        );
+        emit RecoveredERC721(_tokenAddress, _tokenId);
     }
 
-    // Contract's USDC Token lookup function
-    function usdcTokenBalance() public view returns (uint256) {
-        uint256 balance;
-        balance = usdcToken.balanceOf(address(this));
-        return balance;
-    }
-
-    // Contract's USDC Token Transfer Function
-    function transferUsdcToken(uint256 _amount) public onlyOwner {
-        usdcToken.transfer(msg.sender, _amount);
-    }
-
-    /* ==================== Staking Function ==================== */
+    // ------------------ Staking Function ------------------ //
 
     // Staking function
-    function stake(uint256 amount) public {
-        _stake(msg.sender, amount);
+    function stake(uint256 amount) public nonReentrant whenNotPaused {
+        require(amount > 0, "Cannot stake 0");
+        require(stakingToken.balanceOf(msg.sender) != 0, "Do Not Have Token");
+        require((amount + totalSupply) <= tokenVolume, "Too Many Token");
+        require(totalSupply < tokenVolume, "Already Token Full");
+        require(rewardToken.balanceOf(address(this)) > 0, "Reward Token 0");
+        require(
+            rewardToken.balanceOf(address(this)) -
+                (
+                    ((rewardsDuration * (totalSupply * hanTokenPerLpToken)) /
+                        10**18)
+                ) >
+                rewardsDuration * ((amount * hanTokenPerLpToken) / 10**18),
+            "Total amount of rewards is too high"
+        );
+        if (getAmount[msg.sender] > 0) {
+            totalSupply += amount;
+            uint256 stakedTime = (block.timestamp -
+                uint256(getStakingStartTime[msg.sender]));
+            getBalance[msg.sender] +=
+                stakedTime *
+                ((getAmount[msg.sender] * hanTokenPerLpToken) / 10**18);
+            stakingToken.transferFrom(msg.sender, address(this), amount);
+            getAmount[msg.sender] += amount;
+            getStakingStartTime[msg.sender] = block.timestamp;
+            emit Staked(msg.sender, amount);
+        } else {
+            totalSupply += amount;
+            stakingToken.transferFrom(msg.sender, address(this), amount);
+            getAmount[msg.sender] += amount;
+            getStakingStartTime[msg.sender] = block.timestamp;
+            emit Staked(msg.sender, amount);
+        }
     }
 
     // withdrawal function
-    function withdraw(uint256 amount) public {
-        _withdraw(msg.sender, amount);
+    function withdraw(uint256 amount) public nonReentrant {
+        require(amount > 0, "Cannot withdraw 0");
+        require(getAmount[msg.sender] >= amount, "Insufficient Balance");
+        totalSupply -= amount;
+        uint256 stakedTime = (block.timestamp -
+            uint256(getStakingStartTime[msg.sender]));
+        getBalance[msg.sender] +=
+            stakedTime *
+            ((getAmount[msg.sender] * hanTokenPerLpToken) / 10**18);
+        getAmount[msg.sender] -= amount;
+        getStakingStartTime[msg.sender] = block.timestamp;
+        stakingToken.transfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
     }
 
     // Compensation claim function
@@ -156,7 +159,10 @@ contract StakingRakis6 is ReentrancyGuard, Ownable, Pausable {
             block.timestamp - uint256(getStakingStartTime[_user]) != 0,
             "Too Early"
         );
-        require(getBalance[_user] < rewardTokenBalance(), "Short Reward");
+        require(
+            getBalance[_user] < rewardToken.balanceOf(address(this)),
+            "Short Reward"
+        );
         uint256 stakedTime = (block.timestamp -
             uint256(getStakingStartTime[_user]));
         getBalance[_user] +=
@@ -169,65 +175,30 @@ contract StakingRakis6 is ReentrancyGuard, Ownable, Pausable {
         getStakingStartTime[_user] = block.timestamp;
     }
 
-    // Steakings function Detail Logic
-    function _stake(address _user, uint256 _amount)
-        internal
-        nonReentrant
-        whenNotPaused
-    {
-        require(_amount > 0, "Cannot stake 0");
-        require(stakingToken.balanceOf(_user) != 0, "Do Not Have Token");
-        require((_amount + totalSupply) <= tokenVolume, "Too Many Token");
-        require(totalSupply < tokenVolume, "Already Token Full");
-        require(rewardTokenBalance() > 0, "Reward Token 0");
-        if (getAmount[_user] > 0) {
-            totalSupply += _amount;
-            uint256 stakedTime = (block.timestamp -
-                uint256(getStakingStartTime[_user]));
-            getBalance[_user] +=
-                stakedTime *
-                ((getAmount[msg.sender] * hanTokenPerLpToken) / 10**18);
-            stakingToken.transferFrom(_user, address(this), _amount);
-            getAmount[_user] += _amount;
-            getStakingStartTime[_user] = block.timestamp;
-            emit Staked(_user, _amount);
-        } else {
-            totalSupply += _amount;
-            stakingToken.transferFrom(_user, address(this), _amount);
-            getAmount[_user] += _amount;
-            getStakingStartTime[_user] = block.timestamp;
-            emit Staked(_user, _amount);
-        }
-    }
-
-    // withdrawal function detail logic
-    function _withdraw(address _user, uint256 _amount) internal nonReentrant {
-        require(_amount > 0, "Cannot withdraw 0");
-        require(getAmount[_user] >= _amount, "Insufficient Balance");
-        totalSupply -= _amount;
-        uint256 stakedTime = (block.timestamp -
-            uint256(getStakingStartTime[_user]));
-        getBalance[_user] +=
-            stakedTime *
-            ((getAmount[msg.sender] * hanTokenPerLpToken) / 10**18);
-        getAmount[_user] -= _amount;
-        getStakingStartTime[_user] = block.timestamp;
-        stakingToken.transfer(_user, _amount);
-        emit Withdrawn(_user, _amount);
-    }
-
     // Compensation lookup function
     function updateReward() public view returns (uint256) {
         uint256 value;
-        // uint stakedTime = (block.timestamp - uint(getStakingStartTime[msg.sender]));
         value += ((getAmount[msg.sender] * hanTokenPerLpToken) / 10**18);
         return value;
     }
 
+    // Staked time lookup function
+    function stakingTimeData() public view returns (uint256) {
+        if (getAmount[msg.sender] == 0) {
+            return block.timestamp;
+        } else {
+            uint256 stakedTime;
+            stakedTime = (block.timestamp -
+                uint256(getStakingStartTime[msg.sender]));
+            return stakedTime;
+        }
+    }
 
-    /* ==================== EVENTS ==================== */
+    // ------------------ EVENTS ------------------ //
 
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
     event RewardPaid(address indexed user, uint256 reward);
+    event RecoveredERC20(address token, uint256 amount);
+    event RecoveredERC721(address token, uint256 tokenId);
 }
