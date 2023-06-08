7f08dec6dda060ca45173e6d9fb1ab8377210d0b
han-identity <99153528+han-identity@users.noreply.github.com>
2023-06-08 11:44:55 +0900

    Update HanTimelockController.sol

--- contract/governance/HanTimelockController.sol
+++ contract/governance/HanTimelockController.sol
@@ -2,37 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/governance/TimelockController.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HanTimelockController is TimelockController, Ownable {
+contract HanTimelockController is TimelockController {
     constructor(
         uint256 minDelay,
         address[] memory proposers,
         address[] memory executors
     ) TimelockController(minDelay, proposers, executors, msg.sender) {}
-
-    function recoverERC20(
-        address _tokenAddress,
-        uint256 _tokenAmount
-    ) external onlyOwner {
-        IERC20(_tokenAddress).transfer(msg.sender, _tokenAmount);
-        emit RecoveredERC20(_tokenAddress, _tokenAmount);
-    }
-
-    function recoverERC721(
-        address _tokenAddress,
-        uint256 _tokenId
-    ) external onlyOwner {
-        IERC721(_tokenAddress).safeTransferFrom(
-            address(this),
-            msg.sender,
-            _tokenId
-        );
-        emit RecoveredERC721(_tokenAddress, _tokenId);
-    }
-
-    event RecoveredERC20(address token, uint256 amount);
-    event RecoveredERC721(address token, uint256 tokenId);
 }
